2f0660a24d8f83f56ca02fb814839f576711a8b6
Nomthy <85939615+mbalitoh56@users.noreply.github.com>
2025-02-14 10:47:43 +0200

    Update FirstProject.java

--- FirstProject.java
+++ FirstProject.java
@@ -13,11 +13,12 @@ public class FirstProject {
     public static void main(String[] args) {
      String Fname = "Nomthandazo";
      String Sname = "Mavundla";
+     Integer Snumber = 222890657;
      String school= "CPUT";
      String course = "PostGrad Diploma in ICT";
      Integer age = 25; 
     
-     System.out.println("   Student Information   "+'\n'+'\n'+"NAME: "+Fname+'\n'+"SURNAME: "+Sname+'\n'+"AGE: "+age+'\n'+"SCHOOL: "+school+'\n'+"COURSE: "+course);
+     System.out.println("   Student Information   "+'\n'+'\n'+"NAME: "+Fname+'\n'+"SURNAME: "+Sname+'\n'+"AGE: "+age+'\n'++"STUDENT NO: "+Snumber+'\n'+"SCHOOL: "+school+'\n'+"COURSE: "+course);
     }
    
 }
