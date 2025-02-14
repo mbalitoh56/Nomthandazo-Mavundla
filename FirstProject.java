7e8db60e9d3db1431e4f53b131cd221962b8a77e
Nomthy <85939615+mbalitoh56@users.noreply.github.com>
2025-02-14 10:44:46 +0200

    Add files via upload

---
+++ FirstProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstproject;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class FirstProject {
+    
+    public static void main(String[] args) {
+     String Fname = "Nomthandazo";
+     String Sname = "Mavundla";
+     String school= "CPUT";
+     String course = "PostGrad Diploma in ICT";
+     Integer age = 25; 
+    
+     System.out.println("   Student Information   "+'\n'+'\n'+"NAME: "+Fname+'\n'+"SURNAME: "+Sname+'\n'+"AGE: "+age+'\n'+"SCHOOL: "+school+'\n'+"COURSE: "+course);
+    }
+   
+}
