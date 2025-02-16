058c877a30fcbde44ce12810177c142949ff7348
Nomthy <85939615+mbalitoh56@users.noreply.github.com>
2025-02-16 18:53:35 +0200

    Update FirstProject.java

--- FirstProject.java
+++ FirstProject.java
@@ -10,7 +10,7 @@ package firstproject;
  */
 public class FirstProject {
     
-    public static void main(String[] args) {
+     public static void main(String[] args) {
      String Fname = "Nomthandazo";
      String Sname = "Mavundla";
      Integer Snumber = 222890657;
@@ -18,7 +18,41 @@ public class FirstProject {
      String course = "PostGrad Diploma in ICT";
      Integer age = 25; 
     
-     System.out.println("   Student Information   "+'\n'+'\n'+"NAME: "+Fname+'\n'+"SURNAME: "+Sname+'\n'+"AGE: "+age+'\n'++"STUDENT NO: "+Snumber+'\n'+"SCHOOL: "+school+'\n'+"COURSE: "+course);
+     System.out.println("   Student Information   "+'\n'+'\n'+"NAME: "+Fname+'\n'+"SURNAME: "+Sname+'\n'+"AGE: "+age+"STUDENT NO: "+Snumber+'\n'+"SCHOOL: "+school+'\n'+"COURSE: "+course+'\n');
+     
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("    SIMPLE CALCULATOR   "+'\n');
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                return;
+        }
+        System.out.println("The result is: " + result);
+    }
     }
    
-}
